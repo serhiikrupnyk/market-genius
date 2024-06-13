bf3503ec305c1d4d86c88f3f2a827a6346a1fe30
serhiikrupnyk <sergiokrupnik@gmail.com>
2024-06-13 20:01:19 +0300

    [feat] add user type

---
+++ types/users/index.ts
@@ -0,0 +1,8 @@
+export interface UserData {
+    firstName: string;
+    lastName: string;
+    email: string;
+    phoneNumber: string;
+    avatarUrl: string;
+    planStatus: "base" | "advanced" | "premium";
+}
